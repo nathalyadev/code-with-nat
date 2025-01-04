96180887b3d133bfd2dadd64166a44b4bdc7540b
nathalyadev <ns.contato21@gmail.com>
2025-01-03 22:02:09 -0300

    feat: Correção no basename da aplicação

--- web/src/app/shared/configs/routes/index.ts
+++ web/src/app/shared/configs/routes/index.ts
@@ -1,3 +1,3 @@
-const HOME = "";
+const HOME = "/inicio";
 
 export const routesConfigs = { HOME };
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import checker from "vite-plugin-checker";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
-  base: "code-with-nat/",
+  base: "code-with-nat",
   server: {
     host: "0.0.0.0",
   },
